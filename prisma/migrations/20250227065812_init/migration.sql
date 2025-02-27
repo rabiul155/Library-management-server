a8f3e7c4f177ef5c28cd49816ed6592c26a25ca6
rabiul155 <siyamict@gamil.com>
2025-02-27 12:58:49 +0600

    update prisma

--- prisma/migrations/20250227065812_init/migration.sql
+++ prisma/migrations/20250227065812_init/migration.sql
@@ -1,4 +1,18 @@
 -- CreateTable
+CREATE TABLE "books" (
+    "bookId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "publishedYear" INTEGER NOT NULL,
+    "totalCopies" INTEGER NOT NULL,
+    "availableCopies" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("bookId")
+);
+
+-- CreateTable
 CREATE TABLE "members" (
     "memberId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
